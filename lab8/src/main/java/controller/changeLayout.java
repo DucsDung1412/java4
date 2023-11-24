cefbc76140e2d05617a604698b26731b3f1215d4
Dao Duc Dung <daoducdung2000@gmail.com>
2023-11-24 15:31:13 +0700

    Complete 1/2 lab8_2

--- lab8/src/main/java/controller/changeLayout.java
+++ lab8/src/main/java/controller/changeLayout.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class changeLayout
  */
-@WebServlet({ "/change-layout/home", "/change-layout/about", "/change-layout/contact" })
+@WebServlet({ "/change-layout/home", "/change-layout/about", "/change-layout/contact", "/change-layout/faq", "/change-layout/signin", "/change-layout/signup", "/change-layout/signout", "/change-layout/changep", "/change-layout/favorites", "/change-layout/forgot", "/change-layout/profile" })
 public class changeLayout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,13 +27,30 @@ public class changeLayout extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if(request.getRequestURI().contains("home")) {
-			request.getSession().setAttribute("mess", "index");
+			request.getSession().setAttribute("mess", "home");
 		} else if(request.getRequestURI().contains("about")) {
 			request.getSession().setAttribute("mess", "about");
 		} else if(request.getRequestURI().contains("contact")) {
 			request.getSession().setAttribute("mess", "contact");
+		} else if(request.getRequestURI().contains("faq")) {
+			request.getSession().setAttribute("mess", "faq");
+		} else if(request.getRequestURI().contains("signin")) {
+			request.getSession().setAttribute("mess", "signin");
+		} else if(request.getRequestURI().contains("signup")) {
+			request.getSession().setAttribute("mess", "signup");
+		} else if(request.getRequestURI().contains("signout")) {
+			request.getSession().setAttribute("mess", "signout");
+		} else if(request.getRequestURI().contains("favorites")) {
+			request.getSession().setAttribute("mess", "favorites");
+		} else if(request.getRequestURI().contains("changep")) {
+			request.getSession().setAttribute("mess", "changep");
+		} else if(request.getRequestURI().contains("profile")) {
+			request.getSession().setAttribute("mess", "profile");
+		} else if(request.getRequestURI().contains("forgot")) {
+			request.getSession().setAttribute("mess", "forgot");
 		} 
-		response.sendRedirect("../NewFile.jsp");
+//		response.sendRedirect("../NewFile.jsp");
+		response.sendRedirect("../index.jsp");
 	}
 
 	/**
--- lab8/src/main/webapp/layout.jsp
+++ lab8/src/main/webapp/layout.jsp
@@ -9,7 +9,7 @@
 		<title>Insert title here</title>
 	</head>
 	<body>
-	<% 
+	<%-- <% 
 		String lang = ""+session.getAttribute("lang");
 		String s = session.getAttribute("mess") != null ? ""+session.getAttribute("mess") : "index";
 	%>
@@ -28,6 +28,54 @@
 	%>
 		<h1> <fmt:message key="contact.title"/> </h1>
 	<%
+		}
+	%> --%>
+	<%
+		String s = session.getAttribute("mess") != null ? ""+session.getAttribute("mess") : "home";
+		if(s.equals("home")){
+	%>
+			<h1> Home </h1>
+	<%
+		} else if(s.equals("about")){
+	%>
+			<h1> About Us </h1>
+	<%
+		} else if(s.equals("contact")){
+	%>
+			<h1> Contact Us </h1>
+	<%
+		} else if(s.equals("faq")){
+	%>
+			<h1> FAQ </h1>
+	<%	
+		} else if(s.equals("signin")){
+	%>
+			<h1> signin </h1>
+	<%		
+		} else if(s.equals("signup")){
+	%>
+			<h1> signup </h1>
+	<%		
+		} else if(s.equals("signout")){
+	%>
+			<h1> signout </h1>
+	<%		
+		} else if(s.equals("favorites")){
+	%>
+			<h1> favorites </h1>
+	<%		
+		} else if(s.equals("profile")){
+	%>
+			<h1> profile </h1>
+	<%		
+		} else if(s.equals("changep")){
+	%>
+			<h1> change </h1>
+	<%		
+		} else if(s.equals("forgot")){
+	%>
+			<h1> forgot </h1>
+	<%		
 		}
 	%>
 	</body>
